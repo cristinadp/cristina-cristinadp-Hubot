6e7ba97e2d4ba9ba58b2be67e5b71cf86990cd2c
Cristina Del Puerto <cristidp7@gmail.com>
2017-05-22 14:22:27 -0700

    testing3

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -50,14 +50,14 @@ you need to move the contents of module.exports below into the module.exports co
 var choose;
 choose = ["rock", "paper", "scissors"];
 
-var squirrels;
-squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
+// var squirrels;
+// squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
 
 module.exports = function(robot) {
   // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" 
-  return robot.respond(/hi|hello/i, function(msg) {
-    return msg.send("Howdy!");
-  });
+  // return robot.respond(/hi|hello/i, function(msg) {
+  //   return msg.send("Howdy!");
+  // });
 
   // Random Example
   //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
@@ -70,14 +70,14 @@ module.exports = function(robot) {
   // });
 
 
-  robot.respond(/add (.*) and (.*)/i, function(msg) {
-  var a;
-  var b;
-  var c;
-  a = parseInt(msg.match[1]);
-  b = parseInt(msg.match[2]);
-  c = a + b
+//   robot.respond(/add (.*) and (.*)/i, function(msg) {
+//   var a;
+//   var b;
+//   var c;
+//   a = parseInt(msg.match[1]);
+//   b = parseInt(msg.match[2]);
+//   c = a + b
 
-  return msg.reply(a + " plus " + b + " = " + c);
-});
+//   return msg.reply(a + " plus " + b + " = " + c);
+// });
 };
